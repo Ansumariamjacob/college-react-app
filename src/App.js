eb0a9244c14b457a68f3ee89906d4061885f6fbf
ansumariamjacob <ansumariam99@gmail.com>
2022-05-19 22:48:14 +0530

    login

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Login from './component/Login';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 function App() {
   return (
-    <div className="App">
+    <div >
+      <Login/>
   
     </div>
   );
